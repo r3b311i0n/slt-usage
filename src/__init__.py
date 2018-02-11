b25bbdbfd4147bd2c9b3ecd91a60ae4530e33fa5
r3b311i0n <nasashinega@gmail.com>
2018-02-12 02:12:31 +0530

    Add colorama :rainbow:

--- src/__init__.py
+++ src/__init__.py
@@ -2,40 +2,6 @@ import json
 from scraper import Scraper
 
 
-# Taken from https://stackoverflow.com/a/600612/119527 and modified
-
-
-# def _mkdir_p(dir_path):
-#     try:
-#         os.makedirs(dir_path)
-#     except OSError as exc:  # Python >2.5
-#         if exc.errno == errno.EEXIST and os.path.isdir(dir_path):
-#             pass
-#         else:
-#             raise
-#
-#
-# def _safe_open_w(safe_path, mode):
-#     """ Open "path" for writing, creating any parent directories as needed.
-#     """
-#     _mkdir_p(os.path.dirname(safe_path))
-#     return open(safe_path, mode)
-
-
-# Convert image from base64 to PNG.
-# def _convert_img(img_path):
-#     with open(img_path, 'rb') as f:
-#         contents = f.read()
-#         b64 = contents.partition(b',')[2]
-#         pad = len(b64) % 4
-#         b64 += b'=' * pad
-#         print('Decoding image...')
-#         png = codecs.decode(b64.strip(), 'base64')
-#         # print(b64)
-#         with _safe_open_w(capPath, 'w+b') as w:
-#             print('Writing image to disk...')
-#             w.write(png)
-
 def get_settings():
     conf = json.load(open('./tmp/conf.json'))
     user = conf['user']
@@ -44,11 +10,5 @@ def get_settings():
 
 
 if __name__ == '__main__':
-    # capPath = './tmp/cap.png'
-    # argparser = argparse.ArgumentParser()
-    # argparser.add_argument('path', help='Captcha file path')
-    # args = argparser.parse_args()
-    # path = args.path
-    # _convert_img(path)
     settings = get_settings()
     Scraper(settings[0], settings[1])
--- src/scraper/__init__.py
+++ src/scraper/__init__.py
@@ -1,5 +1,6 @@
 import sys
 import threading
+from colorama import init, Fore, Style
 from halo import Halo
 from selenium import webdriver
 from subprocess import call
@@ -12,6 +13,7 @@ _SLT_URL = 'https://www.internetvas.slt.lk/SLTVasPortal-war/application/index.na
 class Scraper(threading.Thread):
     def __init__(self, user, password):
         super().__init__()
+        init()
         self.spinner = Halo(text='Loading', spinner='dots')
         self.user = user
         self.password = password
@@ -19,25 +21,35 @@ class Scraper(threading.Thread):
         self.firefox_options.binary_location = '/bin/firefox'
         self.firefox_options.set_headless(True)
         self.browser = webdriver.Firefox(firefox_options=self.firefox_options)
-        self.show_captcha()
-        self.submit_form()
-        self.show_stats()
+        self._show_captcha()
+        self._submit_form()
+        self._print_stats()
 
     @staticmethod
-    def read_captcha_answer():
-        print('Please enter captcha:')
+    def _read_captcha_answer():
+        print('Please enter captcha:\n')
         answer = sys.stdin.readline()
         return answer
 
-    def show_stats(self):
+    def _print_stats(self):
         remainder = self.browser.find_elements_by_class_name('progress-label')
-        print(f'\nTotal Volume:\n\n{remainder[0].text}\n{remainder[1].text}\n{remainder[2].text}'
-              f'\n\n\nPeak Volume:\n\n{remainder[3].text}\n{remainder[4].text}\n{remainder[5].text}')
+        print(Fore.CYAN +
+              f'\nTotal Volume:\n\n' +
+              Fore.MAGENTA +
+              f'{remainder[0].text}\n'
+              + Style.BRIGHT + Fore.RED + f'{remainder[1].text}\n'
+              + Style.NORMAL + Fore.MAGENTA + f'{remainder[2].text}' +
+              Fore.CYAN +
+              f'\n\n\nPeak Volume:\n\n' +
+              Fore.MAGENTA +
+              f'{remainder[3].text}\n'
+              + Style.BRIGHT + Fore.RED + f'{remainder[4].text}\n'
+              + Style.NORMAL + Fore.MAGENTA + f'{remainder[5].text}')
 
-    def submit_form(self):
-        answer = self.read_captcha_answer()
+    def _submit_form(self):
+        answer = self._read_captcha_answer()
         self.spinner.start()
-        self.browser.find_element_by_xpath('//input[@placeholder="Portal Username eg: CEN2121212"]').\
+        self.browser.find_element_by_xpath('//input[@placeholder="Portal Username eg: CEN2121212"]'). \
             send_keys(self.user)
         self.browser.find_element_by_class_name('ui-password').send_keys(self.password)
         self.browser.find_element_by_xpath('//input[@placeholder="Enter Above Captcha"]').send_keys(answer)
@@ -45,7 +57,7 @@ class Scraper(threading.Thread):
         self.browser.find_element_by_xpath('//input[@value="Sign in"]').click()
         self.spinner.stop()
 
-    def show_captcha(self):
+    def _show_captcha(self):
         self.spinner.start()
         self.browser.get(_SLT_URL)
         elem = self.browser.find_element_by_css_selector('tr > td > img')
