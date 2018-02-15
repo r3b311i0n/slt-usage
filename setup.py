27b469b681b1d722ffa4889ac08d16eebeafdebe
r3b311i0n <nasashinega@gmail.com>
2018-02-15 06:08:03 +0530

    Fix setup.py :snake:

--- setup.py
+++ setup.py
@@ -1,24 +1,26 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='slt-usage',
-    packages=['slt-usage'],
+    name='slt',
+    packages=find_packages(),
+    package_dir={'slt': 'src'},
     version='0.9.0',
     description='Unofficial usage checker for SLT Internet',
     author='Amal Karunarathna',
     author_email='nasashinega@gmail.com',
     url='https://github.com/r3b311i0n/slt-usage',
-    download_url='https://github.com/r3b311i0n/slt-usage/archive/0.9.0.tar.xz',
+    download_url='https://github.com/r3b311i0n/slt-usage/archive/0.9.0.tar.gz',
     keywords=['lk', 'sri lanka', 'internet', 'isp', 'terminal', 'internet usage', 'slt', 'selenium'],
     classifiers=['Development Status :: 4 - Beta',
-                 'Intended Audience :: Sri Lankans',
+                 'Intended Audience :: End Users/Desktop',
+                 'Intended Audience :: Telecommunications Industry',
                  'License :: OSI Approved :: MIT License',
                  'Programming Language :: Python :: 3.6'],
     install_requires=['selenium', 'halo', 'colorama', 'pyxdg', 'Pillow'],
     python_requires='>=3.6',
     entry_points={
         'console_scripts': [
-            'slt-usage=slt-usage:main',
+            'slt=slt:main',
         ],
     },
 )
